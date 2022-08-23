3cab6c0f2f0e353bb11148d306df11a6631b4e9b
rohanday4@gmail.com <rohanday3>
2022-08-23 03:07:25 +0200

    Finished base game

--- game.py
+++ game.py
@@ -20,11 +20,11 @@ pygame.display.set_caption('Snake')
 screen.fill(0)
 snek = Snake()
 food = Food(findEmptySpace(snek))
-pygame.draw.rect(screen, (255, 0, 0), (snek.head[0]*scale, snek.head[1]*scale, scale, scale))
-font = pygame.font.Font('freesansbold.ttf', 32)
-# text = font.render('Game over', True, green, ())
+pygame.draw.rect(screen, (255, 0, 0), (snek.head[0]*scale, snek.head[1]*scale, snek.size, snek.size))
+font = pygame.font.Font('freesansbold.ttf', 128)
 
 run = True
+game_over = False
 while run:
     clock.tick(5)
     # print(snek.body)
@@ -36,24 +36,30 @@ while run:
                 run = False
             elif event.key == pygame.K_g:
                 snek.grow()
-            elif event.key == K_RIGHT:
+            elif event.key == K_RIGHT and (snek.direction != 'left' or snek.length == 0):
                 snek.turn('right')
-            elif event.key == K_LEFT:
+            elif event.key == K_LEFT and (snek.direction != 'right' or snek.length == 0):
                 snek.turn('left')
-            elif event.key == K_UP:
+            elif event.key == K_UP and (snek.direction != 'down' or snek.length == 0):
                 snek.turn('up')
-            elif event.key == K_DOWN:
+            elif event.key == K_DOWN and (snek.direction != 'up' or snek.length == 0):
                 snek.turn('down')
-    snek.move()
-    if snek.isInBody(snek.head):
-        run = False
-    screen.fill(0)
-    pygame.draw.rect(screen, (255, 0, 0), (snek.head[0]*scale, snek.head[1]*scale, snek.scale, snek.scale))
-    for i in range(len(snek.body)):
-        pygame.draw.rect(screen, (255, 0, 0), (snek.body[i][0]*scale, snek.body[i][1]*scale, snek.scale, snek.scale))
-    # draw food if not eaten
-    if snek.isEating(food):
-        food = Food(findEmptySpace(snek))
+    if not game_over:
+        snek.move()
+        if snek.isInBody(snek.head[0], snek.head[1]):
+            game_over=True
+        screen.fill(0)
+        pygame.draw.rect(screen, (38, 38, 174), (snek.head[0]*scale, snek.head[1]*scale, snek.size, snek.size))
+        for i in range(len(snek.body)):
+            pygame.draw.rect(screen, (54, 54, 213), (snek.body[i][0]*scale, snek.body[i][1]*scale, snek.size, snek.size))
+        # draw food if not eaten
+        if snek.isEating(food):
+            food = Food(findEmptySpace(snek))
+            snek.grow()
+        else:
+            pygame.draw.rect(screen, (255, 0, 0), (food.position[0]*scale, food.position[1]*scale, snek.size, snek.size))
     else:
-        pygame.draw.rect(screen, (0, 255, 0), (food.position[0]*scale, food.position[1]*scale, 25, 25))
+        screen.fill(0)
+        text = font.render('Game over', True, (255, 0, 0), (255,255,0))
+        screen.blit(text, (size/2-text.get_width()/2, size/2-text.get_height()/2))
     pygame.display.update()
--- snake.py
+++ snake.py
@@ -1,32 +1,29 @@
 from food import Food
 
 class Snake:
-    def __init__(self) -> None:
+    def __init__(self, scale=5, screen_size=800) -> None:
         self.length = 0
-        self.scale = 25
-        self.head = [80, 80]
+        self.screen_size = screen_size
+        self.scale = scale
+        self.size = scale**2
+        self.head = [(screen_size/scale)//2, (screen_size/scale)//2]
         # body length
         self.body = []
         self.direction = 'right'
 
     def move(self) -> None:
-        # TODO: cant move into body stoopid
-        if self.direction == 'right':
-            self.head[0] += 5 if self.head[0] < (800/5)-5 else -(800/5)+5
-        elif self.direction == 'left':
-            self.head[0] -= 5 if self.head[0] > 0 else -(800/5)+5
-        elif self.direction == 'up':
-            self.head[1] -= 5 if self.head[1] > 0 else -(800/5)+5
-        elif self.direction == 'down':
-            self.head[1] += 5 if self.head[1] < (800/5)-5 else -(800/5)+5
-        # move all body part to the head
-        self.body.insert(0, self.head)
-        for i in range(1,len(self.body)):
-            self.body[i] = self.body[i-1]
-        print(self.body)
-        print(self.length)
+        self.body.insert(0, self.head[::])
         if len(self.body) > self.length:
             del self.body[-1]
+        # TODO: cant move into body stoopid
+        if self.direction == 'right':
+            self.head[0] += 5 if self.head[0] < (self.screen_size/self.scale)-self.scale else -(self.screen_size/self.scale)+self.scale
+        elif self.direction == 'left':
+            self.head[0] -= 5 if self.head[0] > 0 else -(self.screen_size/self.scale)+self.scale
+        elif self.direction == 'up':
+            self.head[1] -= 5 if self.head[1] > 0 else -(self.screen_size/self.scale)+self.scale
+        elif self.direction == 'down':
+            self.head[1] += 5 if self.head[1] < (self.screen_size/self.scale)-self.scale else -(self.screen_size/self.scale)+self.scale
             
         
     def turn(self, direction: str) -> None:
@@ -34,7 +31,6 @@ class Snake:
 
     def isEating(self, Food) -> bool:
         if self.head[0] == Food.position[0] and self.head[1] == Food.position[1]:
-            self.grow()
             return True
         return False
 
@@ -46,4 +42,3 @@ class Snake:
 
     def grow(self) -> None:
         self.length += 1
-        print("grew")
